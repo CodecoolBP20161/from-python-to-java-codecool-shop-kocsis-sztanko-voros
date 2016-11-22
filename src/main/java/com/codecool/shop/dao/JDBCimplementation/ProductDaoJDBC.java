a6c99f764670096958b9b27ee29676be8e514bcc
vbenedek <xbenedek01@gmail.com>
2016-11-22 14:22:31 +0100

    add and find method

--- src/main/java/com/codecool/shop/dao/JDBCimplementation/ProductDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/JDBCimplementation/ProductDaoJDBC.java
@@ -8,11 +8,16 @@ import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
 import javax.sql.RowSet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Currency;
 import java.util.List;
 import java.util.Locale;
+
+import static org.apache.commons.dbutils.DbUtils.closeQuietly;
 
 public class ProductDaoJDBC extends DataBaseAbstraction implements ProductDao {
     SupplierDao supplierDaoJDBC;
@@ -31,12 +36,57 @@ public class ProductDaoJDBC extends DataBaseAbstraction implements ProductDao {
 
     @Override
     public void add(Product product) {
-
+        String query = "INSERT INTO product (id, name, defaultprice, defaultcurrency, description, product_category, supplier) VALUES (?,?,?,?,?,?,?)";
+        Connection conn = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            conn = getConnection();
+            preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1, product.getId());
+            preparedStatement.setString(2, product.getName());
+            preparedStatement.setFloat(3, product.getDefaultPrice());
+            preparedStatement.setString(4, product.getDefaultCurrency().getCurrencyCode());
+            preparedStatement.setString(5, product.getDescription());
+            preparedStatement.setInt(6, product.getProductCategory().getId());
+            preparedStatement.setInt(7, product.getSupplier().getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeQuietly(conn);
+            closeQuietly(preparedStatement);
+        }
     }
 
     @Override
     public Product find(int id) {
-        return null;
+        String query = "SELECT * FROM product WHERE id = ?";
+        Product product = null;
+        Connection conn = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            conn = getConnection();
+            preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String description = rs.getString("description");
+                Float price = rs.getFloat("defaultprice");
+                String currency = rs.getString("defaultcurrency");
+                Integer categoryID = rs.getInt("product_category");
+                ProductCategory category = categoryDaoJDBC.find(categoryID);
+                Integer supplierID = rs.getInt("supplier");
+                Supplier supplier = supplierDaoJDBC.find(supplierID);
+                product = new Product(id, name, price, currency, description, category, supplier);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeQuietly(conn);
+            closeQuietly(preparedStatement);
+        }
+        return product;
     }
 
     @Override
@@ -64,9 +114,8 @@ public class ProductDaoJDBC extends DataBaseAbstraction implements ProductDao {
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            return productList;
         }
+        return productList;
     }
 
     @Override
