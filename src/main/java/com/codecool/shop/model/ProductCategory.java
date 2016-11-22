c9b7279d0b1a5b98a226e96e4e9b64a0851ecfc6
dsztanko <szt.dori@gmail.com>
2016-11-22 13:34:01 +0100

    minor fix

--- src/main/java/com/codecool/shop/model/ProductCategory.java
+++ src/main/java/com/codecool/shop/model/ProductCategory.java
@@ -21,6 +21,7 @@ public class ProductCategory extends BaseModel implements Filter {
     public ProductCategory(int id, String name, String department, String description) {
         super(id, name, description);
         this.department = department;
+        this.products = new ArrayList<>();
     }
 
     public String getDepartment() {
