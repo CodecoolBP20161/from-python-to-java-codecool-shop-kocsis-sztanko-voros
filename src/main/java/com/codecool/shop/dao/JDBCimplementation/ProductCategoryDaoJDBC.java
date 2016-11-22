455281072e1ba8ab41d31faaa4f101a62c6961cb
dsztanko <szt.dori@gmail.com>
2016-11-22 10:01:07 +0100

    creating empty ProductCategoryDaoJDBC childclass

---
+++ src/main/java/com/codecool/shop/dao/JDBCimplementation/ProductCategoryDaoJDBC.java
@@ -0,0 +1,12 @@
+package com.codecool.shop.dao.JDBCimplementation;
+
+/**
+ * Created by dorasztanko on 2016.11.22..
+ */
+public class ProductCategoryDaoJDBC extends DataBaseAbstraction {
+
+    @Override
+    protected String selectAllSQL() {
+        return null;
+    }
+}
