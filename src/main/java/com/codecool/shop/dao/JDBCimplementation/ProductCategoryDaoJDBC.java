ca9f4b969c1bb9e8b2d59260986ffd9eb5669129
dsztanko <szt.dori@gmail.com>
2016-11-22 15:27:37 +0100

    minor fix

--- src/main/java/com/codecool/shop/dao/JDBCimplementation/ProductCategoryDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/JDBCimplementation/ProductCategoryDaoJDBC.java
@@ -61,7 +61,7 @@ public class ProductCategoryDaoJDBC extends DataBaseAbstraction implements Produ
 
             while (rs.next()) {
             productCategory = new ProductCategory(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));
-            products = (ArrayList) productDaoJDBC.getBy(productCategory);
+            products = (ArrayList<Product>) productDaoJDBC.getBy(productCategory);
             productCategory.setProducts(products);
             }
         } catch (SQLException e) {
