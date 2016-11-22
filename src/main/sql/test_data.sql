0e85ae1d466fe7227f8f3d96babcf176b5e54c65
dsztanko <szt.dori@gmail.com>
2016-11-22 10:49:25 +0100

    test data to productcategory added

---
+++ src/main/sql/test_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO productcategory (name, department, description)
+VALUES ('Tablet', 'Hardware', 'A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.');
+
+INSERT INTO productcategory (name, department, description)
+VALUES ('Laptop', 'Hardware', 'A laptop computer, flat mobile computer with a display.');
