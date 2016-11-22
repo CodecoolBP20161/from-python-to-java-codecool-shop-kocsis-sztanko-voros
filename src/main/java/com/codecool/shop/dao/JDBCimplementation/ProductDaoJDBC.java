73c13565d2efddd296a705148d1655433b640bd4
vbenedek <xbenedek01@gmail.com>
2016-11-22 13:43:39 +0100

    getAll method

--- src/main/java/com/codecool/shop/dao/JDBCimplementation/ProductDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/JDBCimplementation/ProductDaoJDBC.java
@@ -1,6 +1,8 @@
 package com.codecool.shop.dao.JDBCimplementation;
 
+import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
@@ -8,9 +10,18 @@ import com.codecool.shop.model.Supplier;
 import javax.sql.RowSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
+import java.util.Locale;
 
 public class ProductDaoJDBC extends DataBaseAbstraction implements ProductDao {
+    SupplierDao supplierDaoJDBC;
+    ProductCategoryDao categoryDaoJDBC;
+
+    public ProductDaoJDBC() {
+        supplierDaoJDBC = new SupplierDaoJDBC();
+        categoryDaoJDBC = new ProductCategoryDaoJDBC();
+    }
 
     @Override
     protected String selectAllSQL() {
@@ -40,8 +51,15 @@ public class ProductDaoJDBC extends DataBaseAbstraction implements ProductDao {
         try {
             while (rs.next()) {
                 Integer id = rs.getInt("id");
+                String name = rs.getString("name");
+                String description = rs.getString("description")
                 Float price = rs.getFloat("defaultprice");
-
+                String currency = rs.getString("defaultcurrency");
+                Integer categoryID = rs.getInt("product_category");
+                categoryDaoJDBC.find(categoryID);
+                Integer supplierID = rs.getInt("supplier");
+                supplierDaoJDBC.find(supplierID);
+                Product product = new Product;
             }
         } catch (SQLException e) {
             e.printStackTrace();
