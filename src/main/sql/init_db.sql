cf4ed1230d56baceb39d149c1b3b1bf70140ada5
dsztanko <szt.dori@gmail.com>
2016-11-22 14:26:38 +0100

    example data added

--- src/main/sql/init_db.sql
+++ src/main/sql/init_db.sql
@@ -13,12 +13,15 @@ CREATE TABLE productcategory
 CREATE TABLE supplier
 (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(40)
+  name VARCHAR(40),
+  description VARCHAR (500)
 );
 
 CREATE TABLE product
 (
   id SERIAL PRIMARY KEY,
+  name VARCHAR(40),
+  description VARCHAR (500),
   defaultPrice FLOAT,
   defaultCurrency VARCHAR(10),
   product_category INT REFERENCES productcategory(id),
--- src/main/sql/test_data.sql
+++ src/main/sql/test_data.sql
@@ -3,3 +3,15 @@ VALUES ('Tablet', 'Hardware', 'A tablet computer, commonly shortened to tablet, 
 
 INSERT INTO productcategory (name, department, description)
 VALUES ('Laptop', 'Hardware', 'A laptop computer, flat mobile computer with a display.');
+
+INSERT INTO supplier (name, description)
+VALUES ('Amazon', 'Digital content and services');
+
+INSERT INTO supplier (name, description)
+VALUES ('Lenovo', 'Computers');
+
+INSERT INTO product (name, description, defaultprice, defaultcurrency, product_category, supplier)
+VALUES ('Amazon Fire', 'Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.', 49.9, 'USD', (SELECT id FROM productcategory WHERE id = 1), (SELECT id FROM supplier WHERE id = 1));
+
+INSERT INTO product (name, description, defaultprice, defaultcurrency, product_category, supplier)
+VALUES ('Lenovo IdeaPad Miix 700', 'Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.', 479, 'USD', (SELECT id FROM productcategory WHERE id = 1), (SELECT id FROM supplier WHERE id = 2));
