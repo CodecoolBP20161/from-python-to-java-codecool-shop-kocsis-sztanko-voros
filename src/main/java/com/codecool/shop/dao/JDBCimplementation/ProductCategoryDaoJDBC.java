96817266183bb84f0eb0857e3a50b3bc494919c7
dsztanko <szt.dori@gmail.com>
2016-11-22 13:22:58 +0100

    ProductCategory found --- method in progress

--- src/main/java/com/codecool/shop/dao/JDBCimplementation/ProductCategoryDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/JDBCimplementation/ProductCategoryDaoJDBC.java
@@ -4,9 +4,14 @@ import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.model.ProductCategory;
 
 import javax.sql.RowSet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.apache.commons.dbutils.DbUtils.closeQuietly;
 
 /**
  * Created by dorasztanko on 2016.11.22..
@@ -21,12 +26,45 @@ public class ProductCategoryDaoJDBC extends DataBaseAbstraction implements Produ
 
     @Override
     public void add(ProductCategory category) {
-
+        String query = "INSERT INTO productcategory (name, department, description) VALUES (?,?,?)";
+        Connection conn = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            conn = getConnection();
+            preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, category.getName());
+            preparedStatement.setString(2, category.getDepartment());
+            preparedStatement.setString(3, category.getDescription());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeQuietly(conn);
+            closeQuietly(preparedStatement);
+        }
     }
 
     @Override
     public ProductCategory find(int id) {
-        return null;
+        String query = "SELECT * FROM productcategory WHERE id = ?";
+        ProductCategory productCategory = null;
+        Connection conn = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            conn = getConnection();
+            preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+            productCategory = new ProductCategory(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeQuietly(conn);
+            closeQuietly(preparedStatement);
+        }
+        return productCategory;
     }
 
     @Override
@@ -46,7 +84,6 @@ public class ProductCategoryDaoJDBC extends DataBaseAbstraction implements Produ
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        System.out.println(listOfProductCategories);
         return listOfProductCategories;
     }
 }
