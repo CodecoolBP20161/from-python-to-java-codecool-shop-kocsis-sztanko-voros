38f619effe2fcc99d067a4d09dbcf81d85bf4251
vbenedek <xbenedek01@gmail.com>
2016-11-23 12:19:58 +0100

    test data changed

--- src/main/sql/test_data.sql
+++ src/main/sql/test_data.sql
@@ -2,7 +2,7 @@ INSERT INTO productcategory (productcategory_name, productcategory_department, p
 VALUES ('Tablet', 'Hardware', 'A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.');
 
 INSERT INTO productcategory (productcategory_name, productcategory_department, productcategory_description)
-VALUES ('Laptop', 'Hardware', 'A laptop computer, flat mobile computer with a display.');
+VALUES ('Phone', 'Hardware', 'A laptop computer, flat mobile computer with a display.');
 
 INSERT INTO supplier (supplier_name, supplier_description)
 VALUES ('Amazon', 'Digital content and services');
@@ -11,7 +11,7 @@ INSERT INTO supplier (supplier_name, supplier_description)
 VALUES ('Lenovo', 'Computers');
 
 INSERT INTO product (product_name, product_description, product_defaultprice, product_defaultcurrency, product_productcategory, product_supplier)
-VALUES ('Amazon Fire', 'Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.', 49.9, 'USD', (SELECT productcategory_id FROM productcategory WHERE productcategory_id = 1), (SELECT supplier_id FROM supplier WHERE supplier_id = 1));
+VALUES ('Amazon Fire', 'Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.', 49.9, 'USD', (SELECT productcategory_id FROM productcategory WHERE productcategory_id = 2), (SELECT supplier_id FROM supplier WHERE supplier_id = 1));
 
 INSERT INTO product (product_name, product_description, product_defaultprice, product_defaultcurrency, product_productcategory, product_supplier)
 VALUES ('Lenovo IdeaPad Miix 700', 'Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.', 479, 'USD', (SELECT productcategory_id FROM productcategory WHERE productcategory_id = 1), (SELECT supplier_id FROM supplier WHERE supplier_id = 2));
