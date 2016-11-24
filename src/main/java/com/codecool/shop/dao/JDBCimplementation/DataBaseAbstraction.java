f35471c4829b1e2bca3c7c4022d134f342fd4a57
dsztanko <szt.dori@gmail.com>
2016-11-24 12:05:17 +0100

    setting abstract DB

--- src/main/java/com/codecool/shop/dao/JDBCimplementation/DataBaseAbstraction.java
+++ src/main/java/com/codecool/shop/dao/JDBCimplementation/DataBaseAbstraction.java
@@ -12,11 +12,16 @@ public abstract class DataBaseAbstraction {
 
     private static final String DATABASE = "jdbc:postgresql://localhost:5432/postgres";
     private static final String DB_USER = "postgres";
-    private static final String DB_PASSWORD = "doca1993";
+    private static final String DB_PASSWORD = "postgres";
 
 
     protected abstract String selectAllSQL();
 
+    protected abstract String addSQL();
+
+    protected abstract String findSQL();
+
+    protected abstract String removeSQL();
 
     protected Connection getConnection() {
         Connection conn = null;
@@ -56,4 +61,20 @@ public abstract class DataBaseAbstraction {
         return rowset;
     }
 
+    public void remove(int id) {
+        String query = removeSQL();
+        PreparedStatement preparedStatement = null;
+        Connection conn = null;
+        try {
+            conn = getConnection();
+            preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeQuietly(conn);
+            closeQuietly(preparedStatement);
+        }
+    }
 }
