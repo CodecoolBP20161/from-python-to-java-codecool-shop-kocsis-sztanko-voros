6f0c72a90ce1f85f0113f04bf20d2352d305aa7d
dsztanko <szt.dori@gmail.com>
2016-12-06 14:13:55 +0100

    catch form on server side

--- src/main/java/com/codecool/shop/controller/RegistrationHandler.java
+++ src/main/java/com/codecool/shop/controller/RegistrationHandler.java
@@ -8,14 +8,18 @@ import spark.Response;
 public class RegistrationHandler {
 
     public static String handleRegistration(Request req, Response res) {
-        JSONObject json = new JSONObject(req.body());
-
-        String salt;
-        salt = SecurityHandler.createSalt();
-        new UserModel.UserBuilder(json.getString("name"),
-                              json.getString("email"),
-                              salt,
-                              SecurityHandler.createHashedPassword(json.getString("password"), salt)).build();
+        System.out.println(req.params("Username"));
+        System.out.println(req.body());
+//        JSONObject json = new JSONObject(req.body());
+//
+//        String salt;
+//        salt = SecurityHandler.createSalt();
+//        new UserModel.UserBuilder(json.getString("name"),
+//                              json.getString("email"),
+//                              salt,
+//                              SecurityHandler.createHashedPassword(json.getString("password"), salt)).build();
+//        System.out.println(json.getString("name"));
+        res.redirect("/");
         return null;
     }
 }
