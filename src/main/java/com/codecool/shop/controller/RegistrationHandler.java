55bb2776c20aa927233d0da9a16ac67dc77c432e
dsztanko <szt.dori@gmail.com>
2016-12-06 14:27:49 +0100

    data from modal extracted

--- src/main/java/com/codecool/shop/controller/RegistrationHandler.java
+++ src/main/java/com/codecool/shop/controller/RegistrationHandler.java
@@ -1,24 +1,18 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.model.UserModel;
-import org.json.JSONObject;
 import spark.Request;
 import spark.Response;
 
 public class RegistrationHandler {
 
     public static String handleRegistration(Request req, Response res) {
-        System.out.println(req.params("Username"));
-        System.out.println(req.body());
-//        JSONObject json = new JSONObject(req.body());
-//
-//        String salt;
-//        salt = SecurityHandler.createSalt();
-//        new UserModel.UserBuilder(json.getString("name"),
-//                              json.getString("email"),
-//                              salt,
-//                              SecurityHandler.createHashedPassword(json.getString("password"), salt)).build();
-//        System.out.println(json.getString("name"));
+        String salt;
+        salt = SecurityHandler.createSalt();
+        new UserModel.UserBuilder(req.queryParams("Username"),
+                                  req.queryParams("Email"),
+                                  salt,
+                                  SecurityHandler.createHashedPassword(req.queryParams("Password"), salt)).build();
         res.redirect("/");
         return null;
     }
