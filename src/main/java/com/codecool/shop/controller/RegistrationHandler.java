a0d3083157fb69ef620dafdf26ae4efe226b301b
dsztanko <szt.dori@gmail.com>
2016-12-06 14:43:09 +0100

    in progress

--- src/main/java/com/codecool/shop/controller/RegistrationHandler.java
+++ src/main/java/com/codecool/shop/controller/RegistrationHandler.java
@@ -1,6 +1,7 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.model.UserModel;
+import org.json.JSONObject;
 import spark.Request;
 import spark.Response;
 
@@ -8,11 +9,15 @@ public class RegistrationHandler {
 
     public static String handleRegistration(Request req, Response res) {
         String salt;
-        salt = SecurityHandler.createSalt();
-        new UserModel.UserBuilder(req.queryParams("Username"),
-                                  req.queryParams("Email"),
-                                  salt,
-                                  SecurityHandler.createHashedPassword(req.queryParams("Password"), salt)).build();
+        System.out.println(req.body());
+        System.out.println(req.queryParams("name"));
+//        JSONObject json = new JSONObject(req.body());
+//        System.out.println(json.getString("name"));
+//        salt = SecurityHandler.createSalt();
+//        new UserModel.UserBuilder(req.queryParams("Username"),
+//                                  req.queryParams("Email"),
+//                                  salt,
+//                                  SecurityHandler.createHashedPassword(req.queryParams("Password"), salt)).build();
         res.redirect("/");
         return null;
     }
--- src/main/resources/public/JS/script.js
+++ src/main/resources/public/JS/script.js
@@ -1,17 +1,18 @@
 $(document).ready(function () {
+    console.log("benne");
     $("#signup_modal").click(function () {
-        $("#form_id").validate();
-        if ($("#form_id").valid()) {
+        // $("#form_id").validate();
+        // if ($("#form_id").valid()) {
             var $name = $("#name").val();
             var $email = $("#email").val();
-            var $password = $("#password").val()
+            var $password = $("#password").val();
+            console.log($name);
             $.post("/registration",
                 {
                     name: $name,
                     email: $email,
                     password: $password
-
                 })
-        }
+
     })
 });
