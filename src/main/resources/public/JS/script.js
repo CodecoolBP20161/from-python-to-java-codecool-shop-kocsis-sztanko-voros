b642a3e44592c1f1e03fe3ce4a0240f812fa88c0
balag3 <carbalage@gmail.com>
2016-12-06 15:06:11 +0100

    Adds json to jquery

--- src/main/resources/public/JS/script.js
+++ src/main/resources/public/JS/script.js
@@ -1,18 +1,23 @@
 $(document).ready(function () {
-    console.log("benne");
     $("#signup_modal").click(function () {
         // $("#form_id").validate();
         // if ($("#form_id").valid()) {
-            var $name = $("#name").val();
-            var $email = $("#email").val();
-            var $password = $("#password").val();
-            console.log($name);
-            $.post("/registration",
-                {
-                    name: $name,
-                    email: $email,
-                    password: $password
-                })
+        var $name = $("#name").val();
+        var $email = $("#email").val();
+        var $password = $("#password").val();
+        var arr = {name: $name, email: $email, password: $password}
+        $.ajax(
+            {
+                url: '/registration',
+                type: 'POST',
+                data: JSON.stringify(arr),
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+                async: true,
+                success: function (msg) {
+                    alert(msg);
+                }
 
+            })
     })
 });
