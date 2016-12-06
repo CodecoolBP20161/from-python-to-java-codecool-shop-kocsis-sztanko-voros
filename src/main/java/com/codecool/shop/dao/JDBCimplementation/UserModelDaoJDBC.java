fb9ec81236926da42f6d31ed0f30c593e0e398eb
dsztanko <szt.dori@gmail.com>
2016-12-06 14:00:12 +0100

    implementation JDBC basic skeleton

---
+++ src/main/java/com/codecool/shop/dao/JDBCimplementation/UserModelDaoJDBC.java
@@ -0,0 +1,28 @@
+package com.codecool.shop.dao.JDBCimplementation;
+
+import com.codecool.shop.dao.UserModelDao;
+import com.codecool.shop.model.UserModel;
+
+import java.util.List;
+
+public class UserModelDaoJDBC implements UserModelDao{
+    @Override
+    public void add(UserModel userModel) {
+
+    }
+
+    @Override
+    public UserModel find(String id) {
+        return null;
+    }
+
+    @Override
+    public void remove(String id) {
+
+    }
+
+    @Override
+    public List<UserModel> getAll() {
+        return null;
+    }
+}
