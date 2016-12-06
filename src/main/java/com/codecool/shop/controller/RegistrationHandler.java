a45d66782a7482a0d6002e5808f90dc1098ba338
dsztanko <szt.dori@gmail.com>
2016-12-06 11:42:13 +0100

    basic skeleton for JSON extracter method

---
+++ src/main/java/com/codecool/shop/controller/RegistrationHandler.java
@@ -0,0 +1,12 @@
+package com.codecool.shop.controller;
+
+import spark.Request;
+import spark.Response;
+
+public class RegistrationHandler {
+
+    public static String handleRegistration(Request req, Response res) {
+        req.body();
+        return null;
+    }
+}
