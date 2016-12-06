e1c5d032cf537880fe4caa7e63840ba91336e537
dsztanko <szt.dori@gmail.com>
2016-12-06 13:30:44 +0100

    handler in progress

--- src/main/java/com/codecool/shop/controller/RegistrationHandler.java
+++ src/main/java/com/codecool/shop/controller/RegistrationHandler.java
@@ -1,12 +1,22 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.model.UserModel;
+import org.json.JSONObject;
 import spark.Request;
 import spark.Response;
 
 public class RegistrationHandler {
 
     public static String handleRegistration(Request req, Response res) {
-        req.body();
+        JSONObject json = new JSONObject(req.body());
+
+        String salt;
+        salt = SecurityHandler.createSalt();
+        new UserModel.UserBuilder(json.getString("name"),
+                              json.getString("email"),
+                              salt,
+                              SecurityHandler.createHashedPassword(json.getString("password"), salt)).build();
         return null;
     }
+
 }
