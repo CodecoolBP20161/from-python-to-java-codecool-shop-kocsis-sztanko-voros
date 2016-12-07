85fbbd60554281d3c89d744db84b2cf1f275bff9
vbenedek <xbenedek01@gmail.com>
2016-12-07 15:39:03 +0100

    spark server for email sender microservice

--- src/main/java/com/codecool/microservices/email_sender_service/EmailSenderService.java
+++ src/main/java/com/codecool/microservices/email_sender_service/EmailSenderService.java
@@ -1,7 +1,38 @@
-package com.codecool.services;
+package com.codecool.microservices.email_sender_service;
 
-/**
- * Created by vbenedek on 2016.12.07..
- */
+import com.codecool.microservices.email_sender_service.controller.MailServiceController;
+import com.codecool.microservices.email_sender_service.service.MailService;
+
+import java.net.URISyntaxException;
+
+import static spark.Spark.exception;
+import static spark.Spark.get;
+import static spark.Spark.port;
+
 public class EmailSenderService {
+
+    private MailServiceController controller;
+
+    public static void main(String[] args) {
+
+        port(60000);
+
+        EmailSenderService application = new EmailSenderService();
+
+        application.controller = new MailServiceController(MailService.getInstance());
+
+        // --- MAPPINGS ---
+        get("/email", application.controller::sendEmail);
+
+        // --- EXCEPTION HANDLING ---
+        exception(URISyntaxException.class, (exception, request, response) -> {
+            response.status(500);
+            response.body(String.format("URI building error, maybe wrong format? : %s", exception.getMessage()));
+        });
+
+        exception(Exception.class, (exception, request, response) -> {
+            response.status(500);
+            response.body(String.format("Unexpected error occurred: %s", exception.getMessage()));
+        });
+    }
 }
