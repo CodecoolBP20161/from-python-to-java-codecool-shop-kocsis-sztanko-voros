8df5570ad485f1c73e7838da7268132c79c238c9
dsztanko <szt.dori@gmail.com>
2016-12-07 09:47:08 +0100

    JSON created

--- src/main/java/com/codecool/shop/controller/RegistrationHandler.java
+++ src/main/java/com/codecool/shop/controller/RegistrationHandler.java
@@ -9,15 +9,12 @@ public class RegistrationHandler {
 
     public static String handleRegistration(Request req, Response res) {
         String salt;
-        System.out.println(req.body());
-        System.out.println(req.queryParams("name"));
-//        JSONObject json = new JSONObject(req.body());
-//        System.out.println(json.getString("name"));
-//        salt = SecurityHandler.createSalt();
-//        new UserModel.UserBuilder(req.queryParams("Username"),
-//                                  req.queryParams("Email"),
-//                                  salt,
-//                                  SecurityHandler.createHashedPassword(req.queryParams("Password"), salt)).build();
+        JSONObject json = new JSONObject(req.body());
+        salt = SecurityHandler.createSalt();
+        new UserModel.UserBuilder(json.getString("name"),
+                                  json.getString("email"),
+                                  salt,
+                                  SecurityHandler.createHashedPassword(json.getString("password"), salt)).build();
         res.redirect("/");
         return null;
     }
