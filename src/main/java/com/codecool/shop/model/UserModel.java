654fa318db18255d2807c2d949687073ce19d741
dsztanko <szt.dori@gmail.com>
2016-12-07 10:09:04 +0100

    setters and getters added to UserModel

--- src/main/java/com/codecool/shop/model/UserModel.java
+++ src/main/java/com/codecool/shop/model/UserModel.java
@@ -4,6 +4,31 @@ public class UserModel {
     private String name;
     private String email;
     private String passwordHash;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public String getPasswordSalt() {
+        return passwordSalt;
+    }
+
     private String passwordSalt;
 
     private UserModel (UserBuilder userBuilder) {
