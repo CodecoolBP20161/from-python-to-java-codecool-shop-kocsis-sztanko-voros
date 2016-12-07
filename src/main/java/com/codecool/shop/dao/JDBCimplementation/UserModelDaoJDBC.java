4d6b6be107ed6e975c1447014d4cb66e962b20fe
balag3 <carbalage@gmail.com>
2016-12-07 10:07:51 +0100

    Adds UserModel Dao JDBC find() by email implementation

--- src/main/java/com/codecool/shop/dao/JDBCimplementation/UserModelDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/JDBCimplementation/UserModelDaoJDBC.java
@@ -2,8 +2,15 @@ package com.codecool.shop.dao.JDBCimplementation;
 
 import com.codecool.shop.dao.UserModelDao;
 import com.codecool.shop.model.UserModel;
+import com.sun.rowset.CachedRowSetImpl;
 
+import javax.sql.rowset.CachedRowSet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
+
+import static org.apache.commons.dbutils.DbUtils.closeQuietly;
 
 public class UserModelDaoJDBC extends DataBaseAbstraction implements UserModelDao{
 
@@ -34,7 +41,35 @@ public class UserModelDaoJDBC extends DataBaseAbstraction implements UserModelDa
 
     @Override
     public UserModel find(String email) {
-        return null;
+        UserModel userModel = null;
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs;
+        CachedRowSet rowset;
+        String sql = findSQL();
+
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, email);
+            rs = stmt.executeQuery();
+            rowset = new CachedRowSetImpl();
+            rowset.populate(rs);
+            while (rowset.next()){
+
+                userModel = new UserModel
+                        .UserBuilder(rowset.getString("user_name"), rowset.getString("user_email"), rowset.getString("user_passwordhash"), rowset.getString("user_passwordsalt"))
+                        .build();
+
+                return userModel;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            closeQuietly(conn);
+            closeQuietly(stmt);
+        }
+        return userModel;
     }
 
     @Override
