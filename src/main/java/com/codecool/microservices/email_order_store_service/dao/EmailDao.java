359081afcc458c9cd2a132f230319f4eb5e623bd
vbenedek <xbenedek01@gmail.com>
2016-12-09 00:27:07 +0100

    store emailorders in memory

--- src/main/java/com/codecool/microservices/email_order_store_service/dao/EmailDao.java
+++ src/main/java/com/codecool/microservices/email_order_store_service/dao/EmailDao.java
@@ -1,4 +1,15 @@
 package com.codecool.microservices.email_order_store_service.dao;
 
+import com.codecool.microservices.email_order_store_service.model.UserEmail;
+
+import java.util.ArrayList;
+
 public interface EmailDao {
+
+    void add(UserEmail userEmail);
+    UserEmail find(String id);
+    void remove(String id);
+    void changeStatus(String id);
+    ArrayList getAll();
+
 }
--- src/main/java/com/codecool/microservices/email_order_store_service/dao/inmemoryimplementation/EmailDaoMem.java
+++ src/main/java/com/codecool/microservices/email_order_store_service/dao/inmemoryimplementation/EmailDaoMem.java
@@ -1,12 +1,43 @@
 package com.codecool.microservices.email_order_store_service.dao.inmemoryimplementation;
 
-public class EmailDaoMem {
+import com.codecool.microservices.email_order_store_service.dao.EmailDao;
+import com.codecool.microservices.email_order_store_service.model.UserEmail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmailDaoMem implements EmailDao{
     private static EmailDaoMem ourInstance = new EmailDaoMem();
 
     public static EmailDaoMem getInstance() {
         return ourInstance;
     }
 
-    private EmailDaoMem() {
+    private static List<UserEmail> DATA = new ArrayList<>();
+
+
+    @Override
+    public void add(UserEmail userEmail) {
+        DATA.add(userEmail);
+    }
+
+    @Override
+    public UserEmail find(String id) {
+        return DATA.stream().filter(t -> t.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    @Override
+    public void remove(String id) {
+        DATA.remove(find(id));
+    }
+
+    @Override
+    public void changeStatus(String id) {
+        find(id).setStatus("done");
+    }
+
+    @Override
+    public ArrayList getAll() {
+        return (ArrayList) DATA;
     }
 }
