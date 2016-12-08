693d4836e10b0b20b1711afdf80d7226b15c4be0
vbenedek <xbenedek01@gmail.com>
2016-12-09 00:29:03 +0100

    store, remove and change emailorder

--- src/main/java/com/codecool/microservices/email_order_store_service/service/EmailStoreService.java
+++ src/main/java/com/codecool/microservices/email_order_store_service/service/EmailStoreService.java
@@ -1,5 +1,13 @@
 package com.codecool.microservices.email_order_store_service.service;
 
+import com.codecool.microservices.email_order_store_service.dao.EmailDao;
+import com.codecool.microservices.email_order_store_service.dao.inmemoryimplementation.EmailDaoMem;
+import com.codecool.microservices.email_order_store_service.model.UserEmail;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 public class EmailStoreService {
     private static EmailStoreService ourInstance = new EmailStoreService();
 
@@ -7,6 +15,25 @@ public class EmailStoreService {
         return ourInstance;
     }
 
-    private EmailStoreService() {
+    private static EmailDao emailDao = EmailDaoMem.getInstance();
+
+    public void storeEmail(String subject, String email, String username) throws IOException {
+        UserEmail userEmail = new UserEmail(subject, email, username);
+        emailDao.add(userEmail);
+    }
+
+    public void removeEmail(String id) {
+        emailDao.remove(id);
+    }
+
+    public void changeEmailStatus(String id) {
+        emailDao.changeStatus(id);
+    }
+
+    public String getAll() {
+        JSONArray jsonArray = new JSONArray(emailDao.getAll());
+        JSONObject json = new JSONObject();
+        json.put("emails", jsonArray);
+        return json.toString();
     }
 }
